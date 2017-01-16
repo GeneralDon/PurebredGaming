d5ab26b37aa9e1493a6313f8d34fdd7f4fb499d3
GeneralDon <generaldon@gmail.com>
2017-01-15 19:55:15 -0500

    Init

---
+++ assets/php/office.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "puresjhh_dbwrite";
+$password = "q8sJk=~bQ6Q4";
+$dbname = "puresjhh_base_info";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$email_address = mysqli_real_escape_string($conn, $_POST['email']);
+
+if (filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
+  die("Invalid email format");
+}
+
+$date = date("Y-m-d H:i:s");
+
+
+$sql = "INSERT INTO Office (email, date)
+VALUES ('$email_address', '$date')";
+
+if ($conn->query($sql) === TRUE) {
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+
+header("Location: https://www.purebredgaming.com/styles/office.html")
+
+?>
