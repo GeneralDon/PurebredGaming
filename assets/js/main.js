2525460686dfea9ce88634f0f9237879f7c77a25
GeneralDon <generaldon@gmail.com>
2017-01-20 15:48:28 -0500

    Removed is-loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,11 +23,11 @@
 			$logoD = $('#logoD');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		//	$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+		//	$window.on('load', function() {
+		//		$body.removeClass('is-loading');
+		//	});
 
 		// CSS polyfills (IE<9).
 			if (skel.vars.IEVersion < 9)
